f6ac3b9336015ec80d75eedd1b6ef15fd07178cc
nicexy <nicepro_yx@163.com>
2019-11-12 17:27:56 +0800

    Consul服务端

---
+++ src/main/java/com/example/consul/TestController.java
@@ -0,0 +1,24 @@
+package com.example.consul;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @GetMapping("check")
+    private String check() {
+        logger.info("health check");
+        return "ok";
+    }
+
+    @GetMapping("hello")
+    public String hello() {
+        logger.info("hello");
+        return "hello from server provider";
+    }
+}
+
